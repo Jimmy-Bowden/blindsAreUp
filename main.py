aa2f6b28dba59806fde80bd4f2dacea36936f4b5
James Bowden JR <jimmy.bowden@edovo.com>
2022-06-09 22:04:42 -0400

    Initial game

---
+++ main.py
@@ -0,0 +1,141 @@
+from functools import total_ordering
+import time
+import datetime
+import json
+import tkinter
+from tkinter import *
+from xml.etree.ElementTree import tostring
+
+file = open("blinds.json")
+data = json.load(file)
+file.close()
+
+state = {
+    "smallBlind": data[0]['small_blind'],
+    "bigBlind": data[0]['big_blind'],
+    "blindDurationMinutes": 1, 
+    "pauseTimer": 0, #In seconds
+    "currentTimer": 0, #In seconds
+    "currentRound": 0,
+    "maxRounds": len(data),
+    "pauseTimerFlag": False
+}
+
+
+def getSmallBlindText(state):
+    return "Small Blind: " + str(state['smallBlind'])
+
+def getBigBlindText(state):
+    return "Big Blind:     " + str(state['bigBlind'])
+
+def getCurrentTimerText(state):
+    return datetime.timedelta(seconds = state['currentTimer'])
+
+def countdown(state):
+    while state['currentTimer'] > 0:
+        if(state['pauseTimerFlag'] == True):
+            # pauseTimerFlag = False
+            return
+        timer = datetime.timedelta(seconds = state['currentTimer'])
+        timerLabel.config(text=timer)
+        master.update()
+        time.sleep(1)
+        state['currentTimer'] -= 1
+    
+    print("Blinds are up!")
+    state['currentRound'] += 1
+    startRound(state)
+
+def startRound(state):
+    state['smallBlind']=data[state['currentRound']]['small_blind']
+    state['bigBlind']=data[state['currentRound']]['big_blind']
+    state['currentTimer']=state['blindDurationMinutes'] * 60
+    smallblindText = getSmallBlindText(state)
+    bigBlindText = getBigBlindText(state)
+
+    smallBlindLabel.config(text=smallblindText)
+    bigBlindLabel.config(text=bigBlindText)
+    master.update()
+
+    countdown(state)
+
+def pauseTimer(state):
+    print("Fix me")
+
+def unpauseTimer(state):
+    print("Fix me")
+
+def nextBlind(state):
+    if(state['currentRound'] < state['maxRounds']):
+        state['currentRound'] += 1
+        restartBlindTimer(state)
+        startRound(state)
+    else:
+        print("Can't go past the max rounds")
+
+def previousBlind(state):
+    if(state['currentRound'] > 0):
+        state['currentRound'] -= 1
+        restartBlindTimer(state)
+        startRound(state)
+    else:
+        print("Can't go into negative rounds")
+
+def restartBlindTimer(state):
+    state['currentTimer'] = state['blindDurationMinutes'] * 60
+    timerLabel.config(text=getCurrentTimerText())
+
+def restartGame(state):
+    state['pauseTimerFlag'] = True
+    restartBlindTimer(state)
+    state['currentRound'] = 0
+    state['smallBlind'] = data[0]["small_blind"]
+    state['bigBlind'] = data[0]["big_blind"]
+    smallBlindLabel.config(text=getSmallBlindText(state))
+    bigBlindLabel.config(text=getBigBlindText(state))
+
+
+master = tkinter.Tk()
+
+canvas_height=1080
+canvas_width=1920
+
+master.geometry(str(canvas_height)+"x"+str(canvas_width))
+master.title="Blinds Are Up Timer"
+
+parentFrame = Frame(master, width=canvas_width, height=canvas_height)
+parentFrame.pack()
+
+labelFrame = Frame(parentFrame, width=canvas_width, height=canvas_height/2)
+labelFrame.pack(side = TOP)
+
+buttonFrame = Frame(parentFrame, width=canvas_width, height=canvas_height/2)
+buttonFrame.pack(side = BOTTOM)
+
+blindValueFrame = Frame(labelFrame, width=canvas_width, height=canvas_height/4)
+blindValueFrame.pack(side = TOP)
+
+timerFrame = Frame(labelFrame, width=canvas_width, height=canvas_height/4)
+timerFrame.pack(side = BOTTOM)
+
+smallBlindLabel = Label(blindValueFrame, text=getSmallBlindText(state))
+smallBlindLabel.config(font=('Helvatical bold',100))
+smallBlindLabel.pack(side = TOP)
+
+bigBlindLabel = Label(blindValueFrame, text=getBigBlindText(state))
+bigBlindLabel.config(font=('Helvatical bold',100))
+bigBlindLabel.pack(side = BOTTOM)
+
+timerLabel = Label(timerFrame, text=str(datetime.timedelta(seconds = state['blindDurationMinutes'] * 60)))
+timerLabel.config(font=('Helvatical bold',300))
+timerLabel.pack(side = BOTTOM)
+
+startBlindsButton = tkinter.Button(buttonFrame, text='Start Game', width=25, command=lambda: startRound(state))
+startBlindsButton.pack(side = TOP)
+
+restartGameButton = tkinter.Button(buttonFrame, text='Restart Game', width=25, command=lambda: restartGame(state))
+restartGameButton.pack(side = BOTTOM)
+
+master.mainloop()
+
+
